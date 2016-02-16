1a54e01d19b0470dfaae9c8a151c5e6ef1c8f0e3
Pankaj Tewani <pankaj.tewani@tothenew.com>
2016-02-16 16:37:11 +0530

    Initial UI & Default App Logo

---
+++ app/src/main/java/com/sample/androidtv/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sample.androidtv.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.sample.androidtv.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/com/sample/androidtv/fragments/MainFragment.java
@@ -0,0 +1,40 @@
+package com.sample.androidtv.fragments;
+
+
+import android.os.Bundle;
+import android.support.v17.leanback.app.BrowseFragment;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sample.androidtv.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends BrowseFragment {
+
+    private static final String TAG = MainFragment.class.getSimpleName();
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.i(TAG, "on Activity Created");
+        super.onActivityCreated(savedInstanceState);
+        initUI();
+    }
+
+    private void initUI(){
+        setTitle(getResources().getString(R.string.app_text));
+        setHeadersState(HEADERS_ENABLED);
+        setHeadersTransitionOnBackEnabled(true);
+        setBrandColor(getResources().getColor(R.color.fastlane_background));
+        setSearchAffordanceColor(getResources().getColor(R.color.search_opaque));
+    }
+}
