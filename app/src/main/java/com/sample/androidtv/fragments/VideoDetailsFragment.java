240ccff9508781b807a06a6294831296369b8e09
Pankaj Tewani <pankaj.tewani@tothenew.com>
2016-02-24 11:33:41 +0530

    Dp utilities functions removed from Details Fragment

--- app/src/main/java/com/sample/androidtv/fragments/VideoDetailsFragment.java
+++ app/src/main/java/com/sample/androidtv/fragments/VideoDetailsFragment.java
@@ -1,8 +1,6 @@
 package com.sample.androidtv.fragments;
 
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -14,7 +12,6 @@ import android.support.v17.leanback.widget.DetailsOverviewRow;
 import android.support.v17.leanback.widget.ListRow;
 import android.support.v17.leanback.widget.SparseArrayObjectAdapter;
 import android.support.v4.app.Fragment;
-import android.util.DisplayMetrics;
 
 import com.sample.androidtv.R;
 import com.sample.androidtv.background.managers.GlideBackgroundManager;
@@ -46,33 +43,6 @@ public class VideoDetailsFragment extends DetailsFragment {
         // Required empty public constructor
     }
 
-    /**
-     * This method converts dp unit to equivalent pixels, depending on device density.
-     *
-     * @param dp      A value in dp (density independent pixels) unit. Which we need to convert into pixels
-     * @param context Context to get resources and device specific display metrics
-     * @return A float value to represent px equivalent to dp depending on device density
-     */
-    public static float convertDpToPixel(float dp, Context context) {
-        Resources resources = context.getResources();
-        DisplayMetrics metrics = resources.getDisplayMetrics();
-        float px = dp * (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
-        return px;
-    }
-
-    /**
-     * This method converts device specific pixels to density independent pixels.
-     *
-     * @param px      A value in px (pixels) unit. Which we need to convert into db
-     * @param context Context to get resources and device specific display metrics
-     * @return A float value to represent dp equivalent to px value
-     */
-    public static float convertPixelsToDp(float px, Context context) {
-        Resources resources = context.getResources();
-        DisplayMetrics metrics = resources.getDisplayMetrics();
-        float dp = px / (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
-        return dp;
-    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
